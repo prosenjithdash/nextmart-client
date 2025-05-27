755f5a64634ba417300a4fc640d0b6d4b38ebea9
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-05-27 13:20:18 +0600

    Updated some folder and add components

---
+++ src/app/(WithCommonLayout)/create-shop/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateShopPage = () => {
+    return (
+        <div>
+            <h1>This is create shop page.</h1>
+        </div>
+    );
+};
+
+export default CreateShopPage;
---
+++ src/app/(WithCommonLayout)/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useUser } from "@/context/UserContext";
+
+// import { useUser } from "../../context/UserContext";
+
+
+
+const HomePage = () => {
+  const user = useUser();
+  console.log(user);
+  return (
+    <div>
+      <h1>Welcome To NextMart Home Page</h1>
+    </div>
+  );
+};
+
+export default HomePage;
